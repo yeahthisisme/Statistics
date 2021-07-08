2268f00f17b5a7992a277c70d9931bcf25b99971
hweei <1154874328@qq.com>
2021-07-08 12:56:36 +0800

    finish

--- chart.vue
+++ chart.vue
@@ -7,10 +7,10 @@
       <el-button size="small" plain>零售商</el-button>
       <el-button size="small" plain>其他用户</el-button>
     </div>
-    <div class="flex wrapper">
+    <div class="space-around wrapper">
       <div>
         <div class="chart" id="echart1" ref="mychart1"></div>
-        <div>
+        <div class="center">
           <el-date-picker
             v-model="value1"
             type="daterange"
@@ -26,7 +26,7 @@
       </div>
       <div>
         <div class="chart" id="echart2" ref="mychart2"></div>
-        <div>
+        <div class="center">
           <el-date-picker
             v-model="value2"
             type="daterange"
@@ -38,6 +38,57 @@
             change="chartChange(2)"
           >
           </el-date-picker>
+        </div>
+      </div>
+    </div>
+    <div class="table-container">
+      <div class="left">数据一览</div>
+      <div class="table">
+        <div class="table-tr">
+          <div class="table-td">
+            <div class="sub-table">
+              <div class="sub-table-tr">
+                <div class="table-th" style="width: 10%"></div>
+                <div class="table-th" style="width: 30%">访客量(UV)</div>
+                <div class="table-th" style="width: 30%">浏览量(PV)</div>
+                <div class="table-th" style="width: 30%">新增用户</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="table-tr">
+          <div class="table-td">
+            <div class="sub-table">
+              <div class="sub-table-tr">
+                <div class="table-th" style="width: 10%"></div>
+                <div class="table-th" style="width: 10%">所有用户</div>
+                <div class="table-th" style="width: 10%">零售商</div>
+                <div class="table-th" style="width: 10%">其他用户</div>
+                <div class="table-th" style="width: 10%">所有用户</div>
+                <div class="table-th" style="width: 10%">零售商</div>
+                <div class="table-th" style="width: 10%">其他用户</div>
+                <div class="table-th" style="width: 10%">所有用户</div>
+                <div class="table-th" style="width: 10%">零售商</div>
+                <div class="table-th" style="width: 10%">其他用户</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="table-tr" v-for="(item, i) in tableList" :key="i">
+          <div class="table-td">
+            <div class="sub-table">
+              <div class="sub-table-tr">
+                <div
+                  :class="{ 'table-th': index == 0, 'sub-table-td': index > 0 }"
+                  style="width: 10%"
+                  v-for="(cell, index) in item"
+                  :key="index"
+                >
+                  {{ cell }}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -60,15 +111,104 @@ module.exports = {
           right: 20,
           orient: "vertical",
           data: [
-            { name: "浏览量（PV）", textStyle: { color: "#86cde5" } },
-            { name: "访客量（UV）", textStyle: { color: "#febb77" } },
+            { name: "浏览量（PV）", textStyle: { color: "#05a3d7" } },
+            { name: "访客量（UV）", textStyle: { color: "#fb5531" } },
           ],
         },
         tooltip: {
           trigger: "axis",
         },
         xAxis: {
-          type: "time",
+          type: "category",
+          axisTick: {
+            alignWithLabel: true,
+          },
+          // data: [
+          //   "2021-07-07",
+          //   "2021-07-08",
+          //   "2021-07-09",
+          //   "2021-07-10",
+          //   "2021-07-11",
+          //   "2021-07-12",
+          //   "2021-07-13",
+          // ],
+          axisLabel: {
+            formatter: function (value, index) {
+              // 格式化成月/日，只在第一个刻度显示年份
+              var date = new Date(value)
+              var texts = [
+                date.getMonth() + 1,
+                date.getDate() < 10 ? "0" + date.getDate() : date.getDate(),
+              ]
+              return texts.join("-")
+            },
+          },
+        },
+        yAxis: [
+          {
+            type: "value",
+            axisLine: {
+              symbol: ["none", "arrow"],
+              symbolOffset: [10, 10],
+            },
+          },
+          {
+            type: "value",
+            axisLine: {
+              symbol: ["none", "arrow"],
+              symbolOffset: [10, 10],
+            },
+          },
+        ],
+        series: [
+          {
+            name: "浏览量（PV）",
+            type: "line",
+            itemStyle: { normal: { color: "#05a3d7", label: { show: true } } },
+            data: [
+              ["2020-10-3", 135],
+              ["2020-10-4", 167],
+              ["2020-10-5", 24],
+              ["2020-10-6", 250],
+              ["2020-10-7", 560],
+              ["2020-10-8", 230],
+              ["2020-10-9", 30],
+              ["2020-10-10", 180],
+            ],
+          },
+          {
+            name: "访客量（UV）",
+            type: "line",
+            itemStyle: { normal: { color: "#fb5531", label: { show: true } } },
+            data: [
+              ["2020-10-3", 240],
+              ["2020-10-4", 110],
+              ["2020-10-5", 40],
+              ["2020-10-6", 150],
+              ["2020-10-7", 330],
+              ["2020-10-8", 130],
+              ["2020-10-9", 80],
+              ["2020-10-10", 180],
+            ],
+          },
+        ],
+      },
+      option2: {
+        title: {
+          text: "小店新增用户",
+          fontWeight: "normal",
+          left: "center",
+        },
+        legend: {
+          right: 20,
+          orient: "vertical",
+          data: [{ name: "新增用户（UV）", textStyle: { color: "#fb5531" } }],
+        },
+        tooltip: {
+          trigger: "axis",
+        },
+        xAxis: {
+          type: "category",
           axisTick: {
             alignWithLabel: true,
           },
@@ -103,24 +243,9 @@ module.exports = {
         ],
         series: [
           {
-            name: "浏览量（PV）",
+            name: "新增用户（UV）",
             type: "line",
-            itemStyle: { normal: { color: "#86cde5", label: { show: true } } },
-            data: [
-              ["2020-10-3", 135],
-              ["2020-10-4", 167],
-              ["2020-10-5", 24],
-              ["2020-10-6", 250],
-              ["2020-10-7", 560],
-              ["2020-10-8", 230],
-              ["2020-10-9", 30],
-              ["2020-10-10", 180],
-            ],
-          },
-          {
-            name: "访客量（UV）",
-            type: "line",
-            itemStyle: { normal: { color: "#febb77", label: { show: true } } },
+            itemStyle: { normal: { color: "#fb5531", label: { show: true } } },
             data: [
               ["2020-10-3", 240],
               ["2020-10-4", 110],
@@ -134,34 +259,17 @@ module.exports = {
           },
         ],
       },
-      option2: {
-        xAxis: {
-          type: "category",
-          data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-        },
-        yAxis: {
-          type: "value",
-        },
-        series: [
-          {
-            data: [150, 230, 224, 218, 135, 147, 260],
-            type: "line",
-          },
-        ],
-      },
-      typePick: "数值",
-      typeList: [
-        {
-          name: "浏览量（PV）",
-        },
-        {
-          name: "访客量（UV）",
-        },
-      ],
-      pagePick: 0,
       // myChart实例
       userChart: {},
       newUserChart: {},
+      tableList: [
+        ["今天", 10, 23, 45, 23, 432, 23, 12, 34, 675],
+        ["昨天", 10, 23, 45, 23, 432, 23, 12, 34, 675],
+        ["本周", 10, 23, 45, 23, 432, 23, 12, 34, 675],
+        ["本月", 10, 23, 45, 23, 432, 23, 12, 34, 675],
+        ["总计", 10, 23, 45, 23, 432, 23, 12, 34, 675],
+        ["自定义时间", 10, 23, 45, 23, 432, 23, 12, 34, 675],
+      ],
     }
   },
   mounted() {
@@ -196,11 +304,13 @@ module.exports = {
 </script>
 
 <style>
-.flex {
-  display: flex;
-}
 .align-center {
   display: flex;
+  align-items: center;
+}
+.center {
+  display: flex;
+  justify-content: center;
   align-items: center;
 }
 .chart-container {
@@ -210,7 +320,6 @@ module.exports = {
   width: 80px;
   font-weight: bold;
   position: relative;
-  float: left;
 }
 .left::before {
   width: 4px;
@@ -223,8 +332,62 @@ module.exports = {
 .wrapper {
   margin-top: 50px;
 }
+.space-around {
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+}
 .chart {
   width: 500px;
   height: 500px;
+}
+.table-container {
+  margin-top: 100px;
+  margin-bottom: 100px;
+}
+.table {
+  display: table;
+  width: 1000px;
+  border-collapse: collapse;
+  margin-top: 40px;
+}
+.table * {
+  margin: 0 auto;
+  padding: 0;
+  font-size: 14px;
+  font-family: Arial, 宋体, Helvetica, sans-serif;
+}
+.table-tr {
+  display: table-row;
+}
+.table-th {
+  display: table-cell;
+  font-weight: bold;
+  height: 34px;
+  border: 1px solid gray;
+  text-align: center;
+  vertical-align: middle;
+  background-color: #f2f2f2;
+}
+.table-td {
+  display: table-cell;
+  height: 34px;
+}
+
+.sub-table {
+  width: 100%;
+  height: 100%;
+  display: table;
+}
+.sub-table-tr {
+  display: table-row;
+  height: 100%;
+}
+.sub-table-td {
+  display: table-cell;
+  height: 100%;
+  border: 1px solid gray;
+  text-align: center;
+  vertical-align: middle;
 }
 </style>
