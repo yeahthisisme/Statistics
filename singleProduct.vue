07f1f2d9363c70b6be84c8cb005d458d114e3b36
hweei <1154874328@qq.com>
2021-07-12 17:21:41 +0800

    update

--- singleProduct.vue
+++ singleProduct.vue
@@ -9,7 +9,7 @@
       <el-menu-item index="1">数据分析</el-menu-item>
       <el-menu-item index="2">查看产品的用户</el-menu-item>
     </el-menu>
-    <div v-show="false">
+    <div v-show="activeIndex == 1">
       <div class="top">
         <div class="bg">
           <div
@@ -69,21 +69,19 @@
         </div>
       </div>
     </div>
-    <div class="table-wrapper">
+    <div class="table-wrapper" v-show="activeIndex == 2">
       <el-row :gutter="10" type="flex" justify="end" align="middle">
         <el-col :span="7">
           用户身份：
           <el-select
-            v-model="value"
-            multiple
+            v-model="selectValue"
             size="small"
             filterable
-            allow-create
             default-first-option
             placeholder="请选择"
           >
             <el-option
-              v-for="item in options"
+              v-for="item in identities"
               :key="item.value"
               :label="item.label"
               :value="item.value"
@@ -110,10 +108,50 @@
             type="index"
             :index="indexMethods"
           ></el-table-column>
+          <el-table-column align="center" width="60">
+            <template slot-scope="scope">
+              <el-avatar :size="50" :src="scope.row.avatar"></el-avatar>
+            </template>
+          </el-table-column>
           <el-table-column
-            label="首次分享"
+            label="用户名称"
             align="center"
-            prop="shareTime"
+            prop="userName"
+          ></el-table-column>
+
+          <el-table-column label="身份" width="100px" align="center">
+            <template slot-scope="scope">
+              <el-tag
+                size="medium"
+                :type="
+                  scope.row.identity == 1
+                    ? 'primary'
+                    : scope.row.identity == 2
+                    ? 'warning'
+                    : scope.row.identity == 3
+                    ? 'success'
+                    : 'info'
+                "
+                >{{ formatType(scope.row.identity) }}</el-tag
+              >
+            </template>
+          </el-table-column>
+          <el-table-column label="手机号" align="center" width="140">
+            <template slot-scope="scope"
+              ><i class="el-icon-phone"></i>{{ scope.row.telephone }}</template
+            >
+          </el-table-column>
+
+          <el-table-column
+            label="店铺"
+            align="center"
+            prop="shopName"
+            :show-overflow-tooltip="true"
+          ></el-table-column>
+          <el-table-column
+            label="最近浏览"
+            align="center"
+            prop="latest"
             width="180"
           >
             <!-- <template slot-scope="scope">
@@ -121,79 +159,41 @@
           </template> -->
           </el-table-column>
           <el-table-column
-            label="分享人"
-            align="center"
-            prop="people"
-          ></el-table-column>
-          <el-table-column align="center" width="90">
-            <template slot-scope="scope">
-              <img
-                :src="scope.row.img"
-                :alt="scope.row.title"
-                style="width: 80px; height: 80px"
-              />
-            </template>
-          </el-table-column>
-          <el-table-column
-            label="产品名称"
-            align="center"
-            prop="title"
-            :show-overflow-tooltip="true"
-          ></el-table-column>
-
-          <el-table-column label="身份" width="100px" align="center">
-            <template slot-scope="scope">
-              <span>{{ formatType(scope.row.productType) }}</span>
-            </template>
-          </el-table-column>
-
-          <el-table-column
-            label="总访客量（UV）"
-            width="100"
-            sortable
-            align="center"
-          >
-            <template slot-scope="scope">
-              <el-button
-                size="mini"
-                type="text"
-                icon="el-icon-view"
-                @click="handleView(scope.row, scope.index)"
-                >{{ scope.row.totalUv }}</el-button
-              >
-            </template>
-          </el-table-column>
-          <el-table-column label="零售商（UV）" width="80" align="center">
-            <template slot-scope="scope">
-              <el-button
-                size="mini"
-                type="text"
-                icon="el-icon-view"
-                @click="handleView(scope.row, scope.index)"
-                >{{ scope.row.uv }}</el-button
-              >
-            </template>
-          </el-table-column>
-          <el-table-column width="100"></el-table-column>
-          <el-table-column
-            label="总浏览量（PV）"
+            label="浏览次数（PV）"
             align="center"
             prop="pv"
-            width="130"
+            sortable
+            width="180"
           ></el-table-column>
+          <el-table-column
+            label="分享量"
+            align="center"
+            prop="share"
+          ></el-table-column>
+          <el-table-column label="查看入口" align="center">
+            <template slot-scope="scope">
+              <div
+                class="center"
+                :style="{ color: formatIsFrom(scope.row.isFrom).color }"
+              >
+                <i
+                  style="font-size: 20px"
+                  :class="formatIsFrom(scope.row.isFrom).icon"
+                ></i>
+                <span
+                  >&nbsp;&nbsp;{{ formatIsFrom(scope.row.isFrom).method }}</span
+                >
+              </div>
+            </template>
+          </el-table-column>
         </el-table>
       </div>
 
       <div class="center" style="margin-top: 50px">
         <el-pagination
-          :background="true"
           :current-page.sync="currentPage"
           :page-size.sync="pageSize"
-          :layout="layout"
-          :page-sizes="pageSizes"
           :total="total"
-          v-bind="$attrs"
-          @size-change="handleSizeChange"
           @current-change="handleCurrentChange"
         />
       </div>
@@ -264,12 +264,12 @@ module.exports = {
           axisLabel: {
             formatter: function (value, index) {
               // 格式化成月/日，只在第一个刻度显示年份
-              var date = new Date(value)
+              var date = new Date(value);
               var texts = [
                 date.getMonth() + 1,
                 date.getDate() < 10 ? "0" + date.getDate() : date.getDate(),
-              ]
-              return texts.join("-")
+              ];
+              return texts.join("-");
             },
           },
         },
@@ -355,12 +355,12 @@ module.exports = {
           axisLabel: {
             formatter: function (value, index) {
               // 格式化成月/日，只在第一个刻度显示年份
-              var date = new Date(value)
+              var date = new Date(value);
               var texts = [
                 date.getMonth() + 1,
                 date.getDate() < 10 ? "0" + date.getDate() : date.getDate(),
-              ]
-              return texts.join("-")
+              ];
+              return texts.join("-");
             },
           },
         },
@@ -418,44 +418,64 @@ module.exports = {
         ],
       },
 
+      // ----------------------------查看产品的用户
       // 表格数据
       list: [
         {
-          shareTime: "2021-05-23 22:23:43",
-          people: "小明",
-          img: "https://youlala.oss-accelerate.aliyuncs.com/lihaoaapp/2021/07/02/eadb78e3-5911-477e-9994-2413c4e15f9ajpg?t=1625208263431",
-          title: "桂林3天2晚桂林3天2晚桂林3天2晚",
-          productType: 3,
-          totalUv: 322,
-          uv: 234,
+          avatar:
+            "https://youlala.oss-accelerate.aliyuncs.com/lihaoaapp/2021/07/02/eadb78e3-5911-477e-9994-2413c4e15f9ajpg?t=1625208263431",
+          userName: "小明",
+          identity: 1,
+          telephone: "18652456453",
+          shopName: "燕归来",
+          latest: "2021-05-23 22:23:43",
           pv: 1134,
+          share: 322,
+          isFrom: "3",
         },
         {
-          shareTime: "2021-04-13 12:23:43",
-          people: "李娜",
-          img: "https://youlala.oss-accelerate.aliyuncs.com/lihaoaapp/2021/05/18/913dae86-b084-48d5-b716-eed9bab48b10jpg?t=1621326254358",
-          title: "桂林3天2晚",
-          productType: 3,
-          totalUv: 142,
-          uv: 34,
+          avatar:
+            "https://youlala.oss-accelerate.aliyuncs.com/lihaoaapp/2021/05/18/913dae86-b084-48d5-b716-eed9bab48b10jpg?t=1621326254358",
+          userName: "李娜",
+          identity: 3,
+          telephone: "13564536666",
+          shopName: "黔贵之旅",
+          latest: "2021-04-13 12:23:43",
           pv: 555,
+          share: 142,
+          isFrom: "4",
         },
       ],
       // 遮罩层
-      // loading: true,
-      // 非多个禁用
-      multiple: true,
-      // 显示搜索条件
-      showSearch: true,
+      loading: false,
       // 总条数
       total: 0,
-    }
+      //用户身份
+      identities: [
+        {
+          label: "所有",
+          value: 0,
+        },
+        {
+          label: "零售商",
+          value: 1,
+        },
+      ],
+      //当前选定值为 “所有”
+      selectValue: 0,
+      //当前页
+      currentPage: 1,
+      // 分页页面大小
+      pageSize: 10,
+      //总页数
+      total: 1,
+    };
   },
   mounted() {
-    const end = new Date()
-    const start = new Date()
-    start.setTime(start.getTime() - 3600 * 1000 * 24 * 7)
-    this.value1 = this.value2 = [start, end]
+    const end = new Date();
+    const start = new Date();
+    start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+    this.value1 = this.value2 = [start, end];
 
     this.submitParam.user = [
       {
@@ -468,76 +488,76 @@ module.exports = {
         startTime: start,
         endTime: end,
       },
-    ]
+    ];
     //1.请求图表数据
     //2.画图
-    this.getChartList()
+    this.getChartList();
 
-    this.getAllTableList()
+    this.getAllTableList();
   },
   updated() {
-    this.drawChart()
+    this.drawChart();
   },
   methods: {
     drawChart(type) {
       if (!this.userChart || !this.newUserChart) {
-        this.setEchart()
+        this.setEchart();
       } else {
-        this.changeChart(type)
+        this.changeChart(type);
       }
     },
     setEchart() {
-      let user = this.$refs.mychart1
-      this.userChart = echarts.init(user)
-      this.userChart.setOption(this.option1)
+      let user = this.$refs.mychart1;
+      this.userChart = echarts.init(user);
+      this.userChart.setOption(this.option1);
 
-      let newUser = this.$refs.mychart2
-      this.newUserChart = echarts.init(newUser)
-      this.newUserChart.setOption(this.option2)
+      let newUser = this.$refs.mychart2;
+      this.newUserChart = echarts.init(newUser);
+      this.newUserChart.setOption(this.option2);
     },
     changeChart(isNew) {
       //isNew为undefined 两个图都更新
       if (isNew != "newUser") {
-        this.userChart.setOption(this.option1)
+        this.userChart.setOption(this.option1);
       }
       if (isNew != "user") {
-        this.newUserChart.setOption(this.option2)
+        this.newUserChart.setOption(this.option2);
       }
     },
     radioChange(e) {
-      console.log(e)
-      console.log(this.submitParam.type)
-      this.getChartList()
+      console.log(e);
+      console.log(this.submitParam.type);
+      this.getChartList();
     },
     dateChange(isNew) {
-      console.log(isNew)
-      console.log(this.value1, this.value2)
+      console.log(isNew);
+      console.log(this.value1, this.value2);
       if (isNew == "user") {
         //this.value1是数组储存startTime,endTime; moment(endTime).format("yyyy-MM-dd")
         this.submitParam.user[0].startTime = moment(this.value1[0]).format(
           "yyyy-MM-dd"
-        )
+        );
         this.submitParam.user[0].endTime = moment(this.value1[1]).format(
           "yyyy-MM-dd"
-        )
+        );
       } else {
         // isNew=="newUser"
         //this.value1是数组储存startTime,endTime; moment(endTime).format("yyyy-MM-dd")
         this.submitParam.user[1].startTime = moment(this.value2[0]).format(
           "yyyy-MM-dd"
-        )
+        );
         this.submitParam.user[1].endTime = moment(this.value2[1]).format(
           "yyyy-MM-dd"
-        )
+        );
       }
-      this.getChartList(isNew)
+      this.getChartList(isNew);
     },
     getChartList(isNew) {
       // this.submitParam
       //axios.get(this.submitParam).then(res=>{})
       // 返回数据更改option1
       //再画一遍图
-      this.drawChart(isNew)
+      this.drawChart(isNew);
     },
     getAllTableList() {
       //请求一览表数据
@@ -546,61 +566,109 @@ module.exports = {
     indexMethods(index) {
       // currentpage当前页码，this.list.length总条数，index索引值
       if (this.list.length < 10) {
-        return this.list.length - index
+        return this.list.length - index;
       } else {
-        return this.list.length - (this.queryParams.pageNum - 1) * 10 - index
+        return this.list.length - (this.queryParams.pageNum - 1) * 10 - index;
       }
     },
     handleSelect(key, keyPath) {
-      console.log(key, keyPath)
-      this.activeIndex = key
+      console.log(key, keyPath);
+      this.activeIndex = key;
       if (key == 2) {
-        this.getList()
+        this.getList();
       }
     },
     /** 查询登录日志 */
     getList() {
-      this.loading = true
-      this.total = this.list.length
-      this.loading = false
+      this.loading = true;
+      this.total = this.list.length;
+      this.loading = false;
     },
     formatType(value) {
       switch (value) {
-        case 1 || "1":
-          value = "单订房"
-          break
-        case 2 || "2":
-          value = "散拼团"
-          break
-        case 3 || "3":
-          value = "小包团"
-          break
-        case 4 || "4":
-          value = "自由行"
-          break
+        case 1:
+        case "1":
+          value = "员工";
+          break;
+        case 2:
+        case "2":
+          value = "零售商";
+          break;
+        case 3:
+        case "3":
+          value = "商家零售";
+          break;
+        case 4:
+        case "4":
+          value = "其他用户";
+          break;
       }
-      return value
+      return value;
+    },
+    formatIsFrom(value) {
+      switch (value) {
+        case 1:
+        case "1":
+          value = {
+            method: "产品海报",
+            color: "#E6A23C",
+            icon: "el-icon-picture",
+          };
+          break;
+        case 2:
+        case "2":
+          value = {
+            method: "微信卡片",
+            color: "#67C23A",
+            icon: "el-icon-copy-document",
+          };
+          break;
+        case 3:
+        case "3":
+          value = {
+            method: "旅拍",
+            color: "#409EFF",
+            icon: "el-icon-camera-solid",
+          };
+          break;
+        case 4:
+        case "4":
+          value = {
+            method: "自主浏览",
+            color: "#F56C6C",
+            icon: "el-icon-mobile-phone",
+          };
+          break;
+      }
+      return value;
     },
     /** 导出按钮操作 */
     handleExport() {
-      const queryParams = this.queryParams
+      const queryParams = this.queryParams;
       this.$confirm("是否确认导出所有数据项?", "警告", {
         confirmButtonText: "确定",
         cancelButtonText: "取消",
         type: "warning",
-      })
-        .then(function () {
-          //调数据接口
-        })
-        .then((response) => {
-          this.download(response.msg)
-        })
+      }).then(function () {
+        //1.调数据接口返回response.msg
+        //2.
+        this.download(response.msg);
+      });
     },
     download(fileName) {
-      window.location.href = baseURL + "/common/download?fileName=" + encodeURI(fileName) + "&delete=" + true;
-    }
+      window.location.href =
+        baseURL +
+        "/common/download?fileName=" +
+        encodeURI(fileName) +
+        "&delete=" +
+        true;
+    },
+    //页面改变
+    handleCurrentChange(e) {
+      console.log(e);
+    },
   },
-}
+};
 </script>
 
 <style>
